92c812dc7a993d79e8591ae218d98a6e58366959
tiger-M1Y5265 <241049@std.hi-joho.ac.jp>
2025-05-16 12:28:32 +0900

    復習問題4

---
+++ src/Review4.java
@@ -0,0 +1,14 @@
+public class Review4 {
+    public static void main(String[] args) {
+        int number = 5;
+        String str = 88 + "88";
+        double PI = 3.141592;
+        PI = 3.14;
+        double circ = 3.14;
+        long a = 1000000000;
+        boolean flg = true;
+        byte b = 35;
+        int num = 0x735;
+        String c = "a";
+    }
+}
