b04bdafdf22893b4b101f75e3fce3293b2864dee
tiger-M1Y5265 <241049@std.hi-joho.ac.jp>
2025-05-26 12:14:32 +0900

    復習問題

---
+++ src/reviews/reviews3/Review28.java
@@ -0,0 +1,23 @@
+package reviews.reviews3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Review28 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
+        System.out.print("宛先を入力>");
+        String to = br.readLine();
+        System.out.print("件名を入力>");
+        String subject = br.readLine();
+        System.out.print("本文を入力>");
+        String body = br.readLine();
+        Email email = new Email();
+        if (subject.isEmpty()) {
+           email.send(to,body);
+        } else {
+            email.send(to, subject, body);
+        }
+    }
+    }
