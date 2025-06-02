d3cef8d487ce0a4c2514908f102b032832cb1eb2
tiger-M1Y5265 <241049@std.hi-joho.ac.jp>
2025-06-02 12:17:55 +0900

    復習問題

---
+++ src/textbook_practice/chapter6/practice1.java
@@ -0,0 +1,24 @@
+package textbook_practice.chapter6;
+
+public class practice1 {
+    public static void main(String[] args) throws Exception {
+        doWarusa();
+        doTogame();
+        callDeae();
+        shwoMondokoro();
+    }
+    public static void doWarusa() {
+        System.out.println("きなこでござる。食えませんがの。");
+    }
+    public static void doTogame() {
+        System.out.println("この老いぼれの目はごまかせませんぞ。");
+    }
+    public static void callDeae() {
+        System.out.println("えぇい、こしゃくな。くせ者だ！であえい！");
+    }
+    public static void shwoMondokoro() throws Exception {
+        System.out.println("飛車さん、角さん。もういいでしょう。");
+        System.out.println("この紋所が目に入らぬか！");
+        doTogame();
+    }
+}
